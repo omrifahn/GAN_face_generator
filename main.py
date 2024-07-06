b8238050a77de12b0c46022fea20e40f8bb3b078
omrifahn <omrifahn@gmail.com>
2024-07-06 21:43:25 +0300

    claude

--- main.py
+++ main.py
@@ -1,16 +1,76 @@
-# This is a sample Python script.
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Generator Network
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(100, 256),
+            nn.ReLU(),
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 784),
+            nn.Tanh()
+        )
 
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), 1, 28, 28)
+        return img
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Discriminator Network
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(784, 512),
+            nn.LeakyReLU(0.2),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
 
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+        return validity
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Initialize networks and optimizers
+generator = Generator()
+discriminator = Discriminator()
+optimizer_G = optim.Adam(generator.parameters(), lr=0.0002)
+optimizer_D = optim.Adam(discriminator.parameters(), lr=0.0002)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Loss function
+adversarial_loss = nn.BCELoss()
+
+# Training loop (simplified)
+for epoch in range(200):
+    for i, (imgs, _) in enumerate(dataloader):
+        # Train Discriminator
+        optimizer_D.zero_grad()
+        real_loss = adversarial_loss(discriminator(imgs), torch.ones(imgs.size(0), 1))
+        z = torch.randn(imgs.size(0), 100)
+        fake_imgs = generator(z)
+        fake_loss = adversarial_loss(discriminator(fake_imgs.detach()), torch.zeros(imgs.size(0), 1))
+        d_loss = real_loss + fake_loss
+        d_loss.backward()
+        optimizer_D.step()
+
+        # Train Generator
+        optimizer_G.zero_grad()
+        g_loss = adversarial_loss(discriminator(fake_imgs), torch.ones(imgs.size(0), 1))
+        g_loss.backward()
+        optimizer_G.step()
+
+    # Save generated images
+    if epoch % 10 == 0:
+        save_image(fake_imgs[:25], f"images/{epoch}.png", nrow=5, normalize=True)
+
+print("Training finished!")
